0522e511a82db52e4a38a676444693e745f20c8c
Amarnath <113051198+AmarnathBheema@users.noreply.github.com>
2022-12-19 14:55:16 +0530

    Update app.py

--- python/app.py
+++ python/app.py
@@ -266,5 +266,5 @@ if __name__ == '__main__':
     #     pass
     
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=9090)
     
